b9b06869e4aa4b4b54f479b6b5206f10562bb403
Sandhya Anand <124878411+naturatigris@users.noreply.github.com>
2025-04-14 23:22:26 +0530

    task-1

---
+++ task-1/factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class TestClass
+{
+    static void  Main(string[] args)
+    {
+    Console.WriteLine("Enter your number:");
+    int factorial=1;
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    for (int i=1;i<=a;i++){
+        factorial*=i;
+
+    }
+    Console.WriteLine(factorial);
+    }
+}
