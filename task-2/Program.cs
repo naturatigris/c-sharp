eda13a0944eb15643e62d6bcadcdcd39ae61328d
Sandhya Anand <124878411+naturatigris@users.noreply.github.com>
2025-04-15 19:18:21 +0530

    task-1

---
+++ task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Person
+{
+   string name;
+   int age; 
+}
+class TestClass
+{
+    static void  Main(string[] args)
+    {
+        Person obj1=new Person();
+        Person obj2=new Person();
+        obj1.name='Sandhya';
+        obj1.age=23;
+        obj2.name='roshni';
+        obj2.age=21;
+        Console.WriteLine("Hello"+obj1.name);
+    }
+}
